faf6cab894ae57e54e13cd48799074bdff338844
juniorafter30 <75785576+juniorafter30@users.noreply.github.com>
2021-03-27 16:40:00 +0100

    changes

--- app.js
+++ app.js
@@ -4,14 +4,19 @@ function ampm(time) {
   if (time.value !== "") {
     let hours = time.split(":")[0];
     let minutes = time.split(":")[1];
-    let $milisecondsValue = hours * 60000 * 60 + minutes * 60100;
+    let $milisecondsValue = hours * 60000 * 60 + minutes * 60000;
+    let displayTime1 = 9 * 60000 * 60 + 15 * 60000;
+    let displayTime2 = 7 * 60000 * 60 + 45 * 60000;
+    let displayTime3 = 6 * 60000 * 60 + 15 * 60000;
+    let displayTime4 = 4 * 60000 * 60 + 45 * 60000;
+    let displayTime5 = 3 * 60000 * 60 + 15 * 60000;
+    let displayTime6 = 1 * 60000 * 60 + 45 * 60000;
     console.log($milisecondsValue);
 
-    let timeInMiliseconds = $milisecondsValue;
-    let h, m, s;
+    let timeInMiliseconds = $milisecondsValue - displayTime1;
+    let h, m;
     h = Math.floor(timeInMiliseconds / 1000 / 60 / 60);
     m = Math.floor((timeInMiliseconds / 1000 / 60 / 60 - h) * 60);
-
     m < 10 ? (m = `0${m}`) : (m = `${m}`);
     h < 10 ? (h = `0${h}`) : (h = `${h}`);
     let suffix = h >= 12 ? "pm" : "am";
@@ -19,7 +24,7 @@ function ampm(time) {
     console.log(`${h}:${m}`);
 
     let displayTime = `${h}:${m}` + " " + suffix + "  suggested";
-    let displayTimeTwo = `${h}:${m}` + " " + suffix + "  suggested";
+    let displayTimeTwo = `${h - 7}:${m - 45}` + " " + suffix + "  suggested";
     const displayTimeThree = `${h}:${m}` + " " + suffix;
     const displayTimeFour = `${h}:${m}` + " " + suffix;
     const displayTimeFive = `${h}:${m}` + " " + suffix;
